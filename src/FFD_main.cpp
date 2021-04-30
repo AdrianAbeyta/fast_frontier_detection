3f93f851cd9fdad3341b4172cce69d417c67a649
AdrianAbeyta <54719810+AdrianAbeyta@users.noreply.github.com>
2021-04-30 00:32:28 -0500

    Fixed small bugs and visualized way-point goal.

--- src/FFD_main.cpp
+++ src/FFD_main.cpp
@@ -1,5 +1,4 @@
 
-
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "geometry_msgs/PoseStamped.h"
@@ -67,17 +66,14 @@ void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 
 }
 
-void SendGoaltoMoveBase(){
+void SendGoaltoMoveBase(int frontier_i ){
 
 //////         Handle Movebase Action       //////
 
     //End exploration if there are no more frontiers. 
     if(f_database->frontier_goals.size() == 0)
 			return;
-
-    // Frontier is a list of points returns the index of the frontier with shortest distace with respect to the robot position. 
-    frontier_i = f_database->UpdateClosestFrontierAverage(*contour);
-
+    
     // Fill out move base msg publisher.
     move_base_msgs::MoveBaseGoal goal_pub;
 	goal_pub.target_pose.header.frame_id = "map";
@@ -90,14 +86,13 @@ void SendGoaltoMoveBase(){
     {
 		if(attempts >= 0)
         {
-			//frontier_i = (rand() % f_database->frontier_goals.size());
-            frontier_i = 1;
+			frontier_i = (rand() % f_database->frontier_goals.size());
 			at_target = false;
 		}
 			attempts++;
     }
 
-    goal_pub.target_pose.pose.position.x = f_database->frontier_goals[frontier_i][0];
+    goal_pub.target_pose.pose.position.x = f_database->frontier_goals[frontier_i][0] - 0.1;
     goal_pub.target_pose.pose.position.y = f_database->frontier_goals[frontier_i][1];
 	geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(0);
     goal_pub.target_pose.pose.orientation = odom_quat;
@@ -107,7 +102,7 @@ void SendGoaltoMoveBase(){
     MoveBaseClient ac("move_base", true);
     
     //If move base server not up wait untill it is. 
-    while(!ac.waitForServer(ros::Duration(45.0)))
+    while(!ac.waitForServer(ros::Duration(15.0)))
     {
 				ROS_INFO("Is the move_base action server up? ");
 	}
@@ -117,7 +112,7 @@ void SendGoaltoMoveBase(){
     marker = f_database->PublishNavGoal(goal_pub);
 	
     //Time to wait for robot to travel to goal before setting new path. 
-    ac.waitForResult(ros::Duration(15.0));
+    ac.waitForResult(ros::Duration(10.0));
     
     //If the robot reaches given goal, rotate in circle and publush new goal. 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) 
@@ -126,7 +121,8 @@ void SendGoaltoMoveBase(){
 		ROS_INFO("The base moved to %f,%f", goal_pub.target_pose.pose.position.x, goal_pub.target_pose.pose.position.y);
 		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(3.14);
 		goal_pub.target_pose.pose.orientation = odom_quat;
-		ac.sendGoal(goal_pub);
+		marker = f_database->PublishNavGoal(goal_pub);
+        ac.sendGoal(goal_pub);
 		ac.waitForResult();
 	}
 
@@ -136,29 +132,34 @@ void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
     
     if(initial_map_FLAG == true)
     {
-    tf::StampedTransform transform;   
-    // Transform laserscan to pointcloud. 
-    listener2->waitForTransform("/scan_multi", "/map", ros::Time(0), ros::Duration(3.5));
-    projector_.transformLaserScanToPointCloud("map",*msg,laser_in_map,*listener2);
-    // Get transform of robot pose to map frame. 
-    robot_transform = tfBuffer_->lookupTransform("base_link","map",ros::Time(0),ros::Duration(3.5));
     
-     //////         FFD         //////
+        // Transform laserscan to pointcloud. 
+        ros::Time t = ros::Time(0); 
+        listener2->waitForTransform("/scan", "/map", t, ros::Duration(3));
+        projector_.transformLaserScanToPointCloud("map",*msg,laser_in_map,*listener2);
+        
+        // Get transform of robot pose to map frame. 
+        robot_transform = tfBuffer_->lookupTransform("base_link","map",ros::Time::now(),ros::Duration(3));
+        
+         //////         FFD         //////
+    
+        // Generate a list of contour points (set resolution of line) from laser scan points
+        contour->GenerateContour( laser_in_map );
+    
+        // Return active area as ------- from current contour
+        contour->UpdateActiveArea( odom_msg, laser_in_map, robot_transform );
+    
+        // Appends new frontiers from contour
+        f_database->ExtractNewFrontier(*contour, global_map);
+        
+        f_database->MaintainFrontiers(*contour, global_map); 
 
-    // Generate a list of contour points (set resolution of line) from laser scan points
-    contour->GenerateContour( laser_in_map );
-    
-    // Return active area as ------- from current contour
-    contour->UpdateActiveArea( odom_msg, laser_in_map, robot_transform );
-    
-    // Appends new frontiers from contour
-    f_database->ExtractNewFrontier(*contour, global_map); //Somtimes this segfaults need to find out why.... timing issue
-    f_database->MaintainFrontiers(*contour, global_map); 
-   
-    // Send shortest frontier point to move base client. Once client is done call back will end. 
-    SendGoaltoMoveBase();
+        // Frontier is a list of points, returns the index of the frontier with shortest distace with respect to the robot position. 
+        frontier_i = f_database->UpdateClosestFrontierAverage(*contour);
+        SendGoaltoMoveBase(frontier_i);
 
     ROS_INFO("LASER: [%f]", (*msg).header.stamp.toSec());
+    
     }
 }
 
@@ -188,11 +189,13 @@ int main(int argc, char **argv){
     contour = new Contour(&n);
     f_database = new FrontierDB(&n);
     
-    tfBuffer_ = new tf2_ros::Buffer ; 
+    tfBuffer_ = new tf2_ros::Buffer; 
     listener = new tf2_ros::TransformListener(*tfBuffer_);
     listener2 = new tf::TransformListener();
+    
+
     ros::Subscriber odom_sub = n.subscribe("/odom", 1, OdomCallback);
-    ros::Subscriber laser_sub = n.subscribe("/scan_multi", 1, LaserCallback);
+    ros::Subscriber laser_sub = n.subscribe("/scan", 1, LaserCallback);
     ros::Subscriber map_sub = n.subscribe("/map", 1, OccupancyMapCallback);
     //ros::Publisher goal_pub = n.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
     ros::Publisher vis_pub = n.advertise<visualization_msgs::Marker>( "waypoint_marker", 0 );
@@ -210,4 +213,3 @@ int main(int argc, char **argv){
     return 0;
 
 }
-
